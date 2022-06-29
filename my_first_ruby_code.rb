1c94c85a5a6a798b7576a3e32995948553641872
haruna kiwooma <har.mullah@gmail.com>
2022-06-29 10:52:04 +0300

    my first ruby code to print a string

---
+++ my_first_ruby_code.rb
@@ -0,0 +1,12 @@
+# how to print on screen
+print "hello world"
+puts "My name is haruna"
+puts"   /|\\   "
+puts"  / | \\"
+puts" /  |  \\ "
+puts"/___|___\\"
+
+puts"There once was a man name Haruna"
+puts"he was 33 years old"
+puts"he really liked the name haruna"
+puts"but didn't like being 33."
