6694404553c9dcad728010c9b5815feea9fc7cc9
haruna kiwooma <har.mullah@gmail.com>
2022-07-05 16:10:14 +0300

    using split fuction and arrays

---
+++ split_and_arrays.rb
@@ -0,0 +1,11 @@
+rock_botton = "Mark kate olson linssay Lohan charlie heen"
+
+puts rock_botton
+puts rock_botton.split
+
+date = "11/17/2013"
+
+puts date.split("/")
+puts date[1]
+puts date[0..4] #this statement includes the fifth charcter
+puts date[0...4] #this statement does not includes the fifth charcter
