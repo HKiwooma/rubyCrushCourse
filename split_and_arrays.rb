66daa3524162c3571a14527728acdde335f3e8d5
haruna kiwooma <har.mullah@gmail.com>
2022-07-06 21:58:09 +0300

    added array examples

--- split_and_arrays.rb
+++ split_and_arrays.rb
@@ -9,5 +9,10 @@ puts date[1]
 puts date[0..4] #this statement includes the fifth charcter
 puts date[0...4] #this statement does not includes the fifth charcter
 
-array_families= ["Bandy","Musa"]
+array_families = ["Bandy","Musa"]
 puts array_families
+
+mixed_soda_type = ["coke", "Mountain Dew", 7, "Orange", 7+9]
+puts mixed_soda_type
+puts mixed_soda_type[4]
+puts mixed_soda_type[-2]
