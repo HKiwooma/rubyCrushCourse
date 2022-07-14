51862be8de2b44f9df0184f55a9f3f4989ae13a1
haruna kiwooma <har.mullah@gmail.com>
2022-07-14 16:20:28 +0300

    added next if loop

--- loops.rb
+++ loops.rb
@@ -30,4 +30,11 @@ loop do
 end
 puts "Game Over"
 
-
+#next if
+any_number = 100
+loop do
+    any_number -= 1
+    next if any_number % 2 != 0
+    puts "#{any_number}"
+    break if any_number <= 0
+end
