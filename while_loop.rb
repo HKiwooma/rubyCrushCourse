fc425ac1c159226bf6d640b4871ad8afe8448fe0
haruna kiwooma <har.mullah@gmail.com>
2022-07-14 15:50:06 +0300

    added while loop

---
+++ while_loop.rb
@@ -0,0 +1,15 @@
+
+#while loop
+
+timer = 10
+while timer >= 0
+    puts timer
+    timer -= 1
+    # timer = timer - 1
+end
+
+counter = -5
+while counter <= 10
+    puts counter
+    counter += 2
+end
