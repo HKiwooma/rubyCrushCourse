47f1a0ac023fdff78dc633c80d3d2521cdee3663
haruna kiwooma <har.mullah@gmail.com>
2022-07-14 11:59:19 +0300

    rounded off to the nearest two decimals

--- pythagoream_theorem.rb
+++ pythagoream_theorem.rb
@@ -9,4 +9,4 @@ b = gets.to_f;
 a2 = a**2
 b2 = b**2
 c2 = a2 + b2
-puts "You look great today! By the way, your triangle's hypotenuse is #{Math.sqrt(c2)}!"
+puts "You look great today! By the way, your triangle's hypotenuse is #{Math.sqrt(c2.round}!"
