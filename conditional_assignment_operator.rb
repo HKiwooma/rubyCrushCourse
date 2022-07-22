c19c613dbbc8516071a253ae6448868d415fe379
haruna kiwooma <har.mullah@gmail.com>
2022-07-22 11:13:44 +0300

    using Conditional Assignmment Operator

---
+++ conditional_assignment_operator.rb
@@ -0,0 +1,9 @@
+# ||= Conditional Assignmment Operator
+
+favorite_animal = nil
+
+favorite_animal ||= "Goat" #It sets a condition equal to something if it is not yet set
+favorite_animal ||= "Emu"
+favorite_animal = "Araripe Manakin"
+
+print favorite_animal 
