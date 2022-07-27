431911c59021baf45d71c79065e397b22ccfedb7
haruna kiwooma <har.mullah@gmail.com>
2022-07-27 09:25:18 +0300

    using branching (if/elsif/else)

--- numbers.rb
+++ numbers.rb
@@ -3,11 +3,15 @@
 puts "Simple Calculator"
 25.times{ print "=" }
 
-puts "\n Enter the first number"
+puts "\nEnter the first number"
 number_1 = gets.chomp
 
-puts "\n Enter the second number"
+puts "Enter the second number"
 number_2 = gets.chomp
+
+puts "What do you want to do?"
+puts "Enter 1 for addition, 2 for Substraction, 3 for Multiplication, 4 for Division, 5 for Modulus"
+user_entry = gets.chomp
 
 #methods
 def add(first_num, second_num)
@@ -26,9 +30,22 @@ def mod(first_num, second_num)
     first_num.to_f % second_num.to_f
 end
 
-#function call outputs
-puts "\n The first number added to second number is #{add(number_1, number_2)}"
-puts "\n The first number subtracted from with second number is #{sub(number_1, number_2)}"
-puts "\n The first number multiplied by second number is #{multiply(number_1, number_2)}"
-puts "\n The first number divided by second number is #{divide(number_1, number_2)}"
-puts "\n The first number mod second number is #{mod(number_1, number_2)}"
+
+if user_entry =="1"
+    puts "you have chosen to add"
+    puts "The first number added to second number is #{add(number_1, number_2)}"
+elsif user_entry =="2"
+    puts "you have chosen to subtract"
+    puts "The first number subtracted from with second number is #{sub(number_1, number_2)}"
+elsif user_entry =="3"
+    puts "you have chosen to multiply"
+    puts "The first number multiplied by second number is #{multiply(number_1, number_2)}"
+elsif user_entry =="4"
+    puts "you have chosen to divide"
+    puts "The first number divided by second number is #{divide(number_1, number_2)}"
+elsif user_entry =="5"
+    puts "you have chosen to modulus"
+    puts "The first number mod second number is #{mod(number_1, number_2)}"
+else
+    puts "Invalid entry"
+end
