87005a9331a768f31cbcdebe19d08eadbbabffb1
haruna kiwooma <har.mullah@gmail.com>
2022-08-18 21:35:41 +0300

    using modules, classes and mixins

--- oop_projects/crud.rb
+++ oop_projects/crud.rb
@@ -3,25 +3,25 @@ module Crud
     puts "Module CRUD activated"
 
     #hashing password
-    def self.create_hash_digest(password)
+    def create_hash_digest(password)
         BCrypt::Password.create(password)
     end
 
     #verify password
-    def self.verify_hash_digest(password)
+    def verify_hash_digest(password)
         BCrypt::Password.new(password) 
     end
 
     #creating secure password
-    def self.create_secure_users(list_of_users)
+    def create_secure_users(list_of_users)
         list_of_users.each do |user_record|
             user_record[:password] = create_hash_digest(user_record[:password])
-        end
+        end 
         list_of_users
     end
 
     #Authenticate users
-    def self.authenticate_user(username, password, list_of_users)
+    def authenticate_user(username, password, list_of_users)
         list_of_users.each do |user_record|
             if user_record[:username] == username && verify_hash_digest(user_record[:password])==password
                 return user_record
